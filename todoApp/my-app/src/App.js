40a0fac5a4fcb6cc1689af0cc0174360c8a751f0
Jeremy Pryor <jeremypryor16@gmail.com>
2022-05-12 09:41:11 -0700

    Week 4 complete

--- todoApp/my-app/src/App.js
+++ todoApp/my-app/src/App.js
@@ -1,16 +1,17 @@
 
 import React, {useState} from 'react'
-import {storeTodos} from './store';
+import {storeTodos} from './components/store';
 import './App.css';
-import TodoList from './TodoList';
-
+import TodoList from './components/TodoList';
+import TodoForm from './components/todoForm';
+import {v4 as uuidv4} from 'uuid'; 
 
 function App() {
 
   const [toDos, setToDos] = useState(storeTodos)
+  const toDoArray = [...toDos];
 
   const completeTodo = (id)=>{
-    const toDoArray = [...toDos];
     const index = toDos.findIndex(toDo => toDo.id === id)
     toDoArray[index].isComplete = !toDoArray[index].isComplete
     setToDos(toDoArray)
@@ -20,9 +21,22 @@ function App() {
     const newToDos = toDos.filter(toDo => toDo.id !== id)
     setToDos(newToDos)
   }
+
+  const addTodo = (text)=>{
+    const todoToAdd = {
+      id: uuidv4(),
+      text: text,
+      isComplete: false
+    }
+    toDoArray.push(todoToAdd)
+    setToDos(toDoArray)
+  }
   
   return (
     <div className="App">
+      <h1>The Greatest List Ever!</h1>
+      <h3>Your Chore List!</h3>
+      <TodoForm addTodo={addTodo}/>
       <TodoList text={toDos} completeTodo={completeTodo} deleteTodo={deleteTodo}/>
     </div>
   );
